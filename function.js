1a84122170105ba5a61f152cb772a323e26a0e7c
BhathiyaTK <bhathiyakariyawasam94@gmail.com>
2018-09-03 10:43:50 +0530

    updated javascript file

--- function.js
+++ function.js
@@ -1,19 +1,16 @@
-var Item1 = getElementById("item1");
-var Item2 = getElementById("item2");
-var Item3 = getElementById("item3");
+var collapses = document.getElementsByClassName("collapse");
 
-Item1.addEventListener('click', item1Click_function());
-Item2.addEventListener('click', item2Click_function());
-Item3.addEventListener('click', item3Click_function());
+for (var i = 0; i < collapses.length; i++) {
+	collapses[i].onclick = function () {
 
-function item1Click_function(){
-	getElementById("para1").css("display","block");
-	getElementById("para2").css.display = "none";
-	getElementById("para3").css.display = "none";
-	getElementById("i1").removeClass("fas fa-plus open");
-	getElementById("i1").addClass("fas fa-minus close");
-	getElementById("i2").removeClass("fas fa-minus close");
-	getElementById("i2").addClass("fas fa-plus open");
-	getElementById("i3").removeClass("fas fa-minus close");
-	getElementById("i3").addClass("fas fa-plus open");
+		this.classList.toggle('active');
+
+		var content = this.nextElementSibling;
+
+		if (content.style.maxHeight) {
+			content.style.maxHeight = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+		}
+	}
 }
